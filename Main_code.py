333a7d9734b9ef66c4b99c23e47def0b75e9784c
Alfie Chen <baomeisha@icloud.com>
2020-04-10 17:16:12 -0400

    map_and_lazer.py splitted into two separate files.

--- Main_code.py
+++ Main_code.py
@@ -1,5 +1,5 @@
 from read_and_export import read_bff, generate_png
-from map import generate_next
+from map import generate_next_map
 from laser import check_intersection, generate_lazer
 
 
@@ -42,10 +42,11 @@ def solve_bff(filename):
         filename)
     initial_laser_path = [(initial_laser[0], initial_laser[1])]
     current_lazer_path = initial_laser_path
-    map_history = []
+    map_history = [initial_map]
 
     while check_intersection(current_lazer_path, required_intersection):
-        current_map = generate_next(initial_map, available_dict, map_history)
+        current_map = generate_next_map(
+            initial_map, available_dict, map_history)
         current_lazer_path = generate_lazer(current_map, initial_laser)
 
     generate_png(filename, current_map, current_lazer_path)
--- map_and_lazer.py
+++
@@ -1,1 +0,0 @@
-# This part is written by Alfie
